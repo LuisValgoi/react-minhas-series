42306ef416fff0f6e6973f9d558529baf3536511
Luis Valgoi <luis.valgoi@sap.com>
2019-10-10 10:18:06 -0300

    created initial request to be

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "./Header";
 import { 
   BrowserRouter as Router, 
@@ -13,6 +14,13 @@ const Generos = () => {
 };
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios.get("/api").then(res => {
+      setData(res.data)
+    });
+  }, []);
+
   return (
     <div>
       <Router>
@@ -20,6 +28,7 @@ function App() {
           <Header/>
           <Route path="/" exact component={Home} />
           <Route path="/generos" component={Generos} />
+          <pre>{JSON.stringify(data)}</pre>
         </div>
       </Router>
     </div>
