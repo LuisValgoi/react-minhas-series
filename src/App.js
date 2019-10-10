bad2130076db97c298228d4999c0a4989eb05b99
Luis Valgoi <luis.valgoi@sap.com>
2019-10-10 09:49:04 -0300

    created Header Component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import React, { useState } from 'react';
-import { 
-  Navbar,
-  NavbarBrand,
-  Collapse,
-  Nav,
-  NavItem,
-  NavLink,
-  NavbarToggler
- } from 'reactstrap';
+import React from "react";
+import Header from "./Header";
 
 function App() {
-  const [open, setOpen] = useState(false);
-  const toggle = () => {
-    setOpen(!open);
-  }
-
   return (
     <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand>Minhas Séries</NavbarBrand>
-        <NavbarToggler onClick={toggle}/>
-        <Collapse isOpen={open} navbar>
-          <Nav className="ml-auto">
-            <NavItem>
-              <NavLink href="/">Genêros</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+      <Header/>
     </div>
   );
 }
