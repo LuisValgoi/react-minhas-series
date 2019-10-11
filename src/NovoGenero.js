9a58e63d4a05805566e41ca929bc978117e8cfd2
Luis Valgoi <luis.valgoi@sap.com>
2019-10-11 20:29:08 -0300

    implemented post of generos

--- src/NovoGenero.js
+++ src/NovoGenero.js
@@ -1,15 +1,34 @@
-import React, { useState }from 'react';
+import React, { useState } from 'react';
+import { Redirect } from "react-router-dom"
+import axios from "axios";
 
 const NovoGenero = () => {
+  const [name, setName] = useState("");
+  const [success, setSuccess] = useState(false);
+  const onChange = (event) => {
+    setName(event.target.value);
+  }
+  const onSave = () => {
+    axios.post("/api/genres", {
+      name: name
+    }).then(() => {
+      setSuccess(true);
+    });
+  };
+
+  if(success) {
+    return <Redirect to="/generos"/>
+  }
+
   return (
     <div className="container">
       <h1>Novo Genêros</h1>
       <form>
         <div className="form-group">
-          <label for="name">Nome do Genêro</label>
-          <input type="text" className="form-control" id="name" placeholder="Nome do Gênero" />
+          <label htmlFor="name">Nome do Genêro</label>
+          <input type="text" value={name} onChange={onChange} className="form-control" id="name" placeholder="Nome do Gênero" />
         </div>
-        <button type="button" class="btn btn-primary">Salvar</button>
+        <button type="button" onClick={onSave} className="btn btn-primary">Salvar</button>
       </form>
     </div>
   );
