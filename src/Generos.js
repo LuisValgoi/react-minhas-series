6ed58f41e03f1a1c6a587545aae1b314b3c01791
Luis Valgoi <luis.valgoi@sap.com>
2019-10-11 20:44:16 -0300

    implemented delete fn from generos

--- src/Generos.js
+++ src/Generos.js
@@ -9,13 +9,20 @@ const Generos = () => {
       setData(res.data.data);
     });
   }, []);
+  
+  const deleteGenero = (id) => {
+    axios.delete("/api/genres/" + id).then((res) => {
+      const filtrado = data.filter(res => res.id !== id);
+      setData(filtrado)
+    });
+  };
 
   const renderizaLinha = record => {
     return (
       <tr key={record.id}>
         <th scope="row">{record.id}</th>
         <td>{record.name}</td>
-        <td><button>+</button></td>
+        <td><button onClick={() => deleteGenero(record.id)}>-</button></td>
       </tr>
     );
   }
