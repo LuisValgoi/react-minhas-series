47c678426ce24e38f225737835abe2390927027e
Luis Valgoi <luis.valgoi@sap.com>
2019-10-11 23:59:09 -0300

    implemented switch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import NovoGenero from "./NovoGenero";
 import EditarGenero from "./EditarGenero";
 import { 
   BrowserRouter as Router, 
-  Route
+  Route,
+  Switch
 } from "react-router-dom";
 
 const Home = () => {
@@ -26,10 +27,12 @@ function App() {
       <Router>
         <div>
           <Header/>
-          <Route path="/" exact component={Home} />
-          <Route path="/generos/:id" exact component={EditarGenero} />
-          <Route path="/generos/novo" exact component={NovoGenero} />
-          <Route path="/generos" exact component={Generos} />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/generos" exact component={Generos} />
+            <Route path="/generos/novo" exact component={NovoGenero} />
+            <Route path="/generos/:id" exact component={EditarGenero} />
+          </Switch>
           <pre>{JSON.stringify(data)}</pre>
         </div>
       </Router>
